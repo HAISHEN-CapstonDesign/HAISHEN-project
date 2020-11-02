789311bb622cc3f11fa005f65585ef80a102761a
Yujin Lee <zz9974@ajou.ac.kr>
2020-11-02 21:06:52 +0900

    change \ -> /

--- frontend/src/views/ProjectStart.vue
+++ frontend/src/views/ProjectStart.vue
@@ -11,7 +11,7 @@
         v-else
         max-height="200"
         max-width="100%"
-        src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+        src="../assets/banner.jpg"
         ></v-img>
     <br>
     <v-container style="width: 60%;">
