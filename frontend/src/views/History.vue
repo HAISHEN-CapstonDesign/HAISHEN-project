602ec86387a13bf8d676bd64f4cd2a3038978025
Yujin Lee <zz9974@ajou.ac.kr>
2020-11-18 20:29:41 +0900

    time

--- frontend/src/views/History.vue
+++ frontend/src/views/History.vue
@@ -30,7 +30,7 @@
                             <v-divider></v-divider>
                             <v-list-item>
                                 <v-list-item-content style="flex-basis: 25%;">
-                                    <v-list-item-title v-text="tmp.time"></v-list-item-title>
+                                    <v-list-item-title v-text="tmp.time.toLocaleString()"></v-list-item-title>
                                 </v-list-item-content>
                                 <v-list-item-content style="flex-basis: 30%;">
                                     <v-list-item-title v-text="tmp.commit_comment"></v-list-item-title>
