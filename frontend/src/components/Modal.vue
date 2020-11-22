a91c45e650c4c2558a6b0ddffdfb70008565716f
Yujin Lee <zz9974@ajou.ac.kr>
2020-11-22 20:32:13 +0900

    data

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -47,11 +47,11 @@ export default {
     },
     fileChange(e) { // eslint-disable-line no-unused-vars
       const file = this.$refs.file.files[0]; 
-      //this.imgfile = file;
+      this.imgfile = file;
       const uploadUrl = `https://httpbin.org/post`;
       let formData = new FormData();
       formData.append("file", this.file);
-      this.imgfile = formData;
+      //this.imgfile = formData;
       axios.post(uploadUrl).then(data => { // eslint-disable-line no-unused-vars
         this.imageSrc = URL.createObjectURL(file);
       });
--- frontend/src/views/BasicCollaboTool.vue
+++ frontend/src/views/BasicCollaboTool.vue
@@ -173,7 +173,6 @@ export default {
               time:'',
               commit_comment:'',
               files:[],
-              test:null,
             },
         }
     },
@@ -189,7 +188,8 @@ export default {
         axios.post(`http://localhost:3000/api/project/${this.idp}/modify/basicTool/${this.ids}`, this.subObj,
           {
             headers: {
-              'token': localStorage.getItem('access_token')
+              'token': localStorage.getItem('access_token'),
+              'Content-Type':'multipart/form-data'
             }
           })
         .then((res) => {
