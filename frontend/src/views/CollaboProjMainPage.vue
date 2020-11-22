90091a8a1e029cb03c2ab699b2788df97d165bb9
gnsals0904 <gnsals0904@naver.com>
2020-11-23 05:18:46 +0900

    collaboprojmainpage editing

--- frontend/src/views/CollaboProjMainPage.vue
+++ frontend/src/views/CollaboProjMainPage.vue
@@ -2,12 +2,24 @@
     <v-app>
         <v-row>
             <v-img
-            class="white--text align-end"
-            height="200px"
-            src="../assets/banner2.jpg"
-            gradient="to top right, rgba(150,150,150,.33), rgba(52,52,52,.7)"
+            class="white--text"
+            max-height="200"
+            max-width="100%"
+            src="../assets/partership.jpg"
+            gradient="to top right, rgba(150,150,150,.60), rgba(52,52,52,.7)"
             >
-            <p style="position: absolute; top: 30%; left:45%; font-size:50px;">{{title}}</p>
+            <v-row
+                class="fill-height"
+                align="center"
+                justify="center"
+                >
+                <v-col
+                align="center"
+                justify="center"
+                >
+                <div class="display-1">{{this.title}}</div>
+                </v-col>
+                </v-row>
             </v-img>
 
         </v-row>
@@ -17,13 +29,11 @@
                     <v-card min-height="500px">
                         <v-col align="center">
                             <v-btn text @click="mainInfo">소개</v-btn>
-                            <v-btn text @click="writerInfo">저자 info</v-btn>
-                            <v-btn text @click="subtitleList">목차</v-btn>
                             <v-btn text @click="supporter">서포터</v-btn>
                         </v-col>
                         <v-divider></v-divider>
                         <v-card>
-                            <div v-show="nowList=='main'">대충 프로젝트 소개하는 내용</div>
+                            <div v-show="nowList=='main'">{{this.introduction}}</div>
                             <Writer v-show="nowList == 'writer'"></Writer>
                             <div v-show="nowList=='subtitle'">프로젝트  목차 나열 나중에 편집기능도 넣어야 할듯</div>
                             <div v-show="nowList=='supporter'">프로젝트 서포터 리스트 component 제작필요</div>
