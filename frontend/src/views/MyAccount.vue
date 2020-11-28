d07ac3dd362be1a761eba11a3a4c25054b14ec34
gnsals0904 <gnsals0904@naver.com>
2020-11-29 02:51:07 +0900

    myaccount page design complete

--- frontend/src/views/MyAccount.vue
+++ frontend/src/views/MyAccount.vue
@@ -1,32 +1,58 @@
 <template>
-    <v-container fill-height style="max-width:1050px;">
+    <v-container fill-height style="max-width:730px;">
     <v-layout align-center row wrap class="mt-15">
-    <v-row no-gutters>
-        <v-col
-        v-for="( item , i ) in account"
-        :key="i"
-        >
+        <v-flex xs12>      
             <v-card
-            width="150"
-            height="150"
-            outlined="false"
-            hover="true"
-            class="text-center"
-            @click="$router.push('/postlist')"
+            color="blue-grey darken-4"
+            class="pb-5"
             >
-                <div style="position: absolute; top: 40%; width:100%">
-                <h3>test</h3>
+                <div class="ml-15">
+                   
+                    <v-col
+                    v-for="( item , i ) in account"
+                    :key="i"
+                    class="ml-15"
+                    >
+                        <v-card
+                        color="yellow darken-1"
+                        width="450"
+                        height="150"
+                        outlined="false"
+                        hover="true"
+                        class="text-center"
+                        @click="$router.push('/postlist')"
+                        >
+                            <div style="position: absolute; top: 40%; width:100%">
+                            <h3>등록된 계좌가 없습니다</h3>
+                            </div>
+                        </v-card>
+                    </v-col>
+                    <div class="ml-16">
+                        <v-card
+                        color="blue-grey darken-3"
+                        width="450"
+                        height="70"
+                        outlined="false"
+                        hover="true"
+                        class="text-center ml-2"
+                        >
+                            <div style="position: absolute; top: 40%; width:100%">
+                            <v-icon>
+                                mdi-plus
+                            </v-icon>
+                            </div>
+                        </v-card>
+                    </div>
                 </div>
             </v-card>
-        </v-col>
-    </v-row>
+        </v-flex>
     </v-layout>
 </v-container>
 </template>
 <script>
 export default {
     data: () => ({
-      account: [],
+      account: ['start'],
     }),
     methods: {
         
