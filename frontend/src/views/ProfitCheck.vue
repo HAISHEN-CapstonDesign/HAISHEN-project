ce3ebd2696f898cbbe09a8c2ca8ac8a1571b02f9
gnsals0904 <gnsals0904@naver.com>
2020-11-29 21:10:48 +0900

    adding table into expansion panel

--- frontend/src/views/ProfitCheck.vue
+++ frontend/src/views/ProfitCheck.vue
@@ -32,19 +32,27 @@
             width="1050"
         >
             <v-expansion-panel 
-            v-for="(item,i) in 5"
+            v-for="(item,i) in project"
             :key="i"
             >
             <v-expansion-panel-header>
-               item
+               {{item}} 총 수익 금액 10000 원 최종 정산일 : 2020.12.12 이번달 정산 : 5000 원
             </v-expansion-panel-header>
             <v-expansion-panel-content>
                 <v-container>
                 <v-card
-                width="100"
-                height="100"
+                width="1050"
+                height="100%"
                 color="cyan"
                 >
+                <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    :sort-by="['calories', 'fat']"
+                    :sort-desc="[false, true]"
+                    multi-sort
+                    class="elevation-1"
+                ></v-data-table>
                 <v-btn
                     @click="overlay = !overlay"
                 >btn
@@ -90,6 +98,103 @@ export default {
     {
         banner_src:require('@/assets/watch.jpg'),
         overlay: false,
+        project_num: 5,
+        project:['project 1', 'project 2', 'project 3', 'project 4', 'project 5'],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [
+          {
+            name: 'Frozen Yogurt',
+            calories: 200,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%',
+          },
+          {
+            name: 'Ice cream sandwich',
+            calories: 200,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%',
+          },
+          {
+            name: 'Eclair',
+            calories: 300,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%',
+          },
+          {
+            name: 'Cupcake',
+            calories: 300,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%',
+          },
+          {
+            name: 'Gingerbread',
+            calories: 400,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%',
+          },
+          {
+            name: 'Jelly bean',
+            calories: 400,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%',
+          },
+          {
+            name: 'Lollipop',
+            calories: 400,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%',
+          },
+          {
+            name: 'Honeycomb',
+            calories: 400,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%',
+          },
+          {
+            name: 'Donut',
+            calories: 500,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%',
+          },
+          {
+            name: 'KitKat',
+            calories: 500,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%',
+          },
+        ],
     }),
     methods:{
         endOver(over){
