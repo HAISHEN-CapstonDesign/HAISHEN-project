25a1541754f3b817eaaee5852a106afafcc612b8
Yujin Lee <zz9974@ajou.ac.kr>
2020-12-04 04:48:13 +0900

    community

--- frontend/src/views/Community.vue
+++ frontend/src/views/Community.vue
@@ -12,7 +12,7 @@
         </v-col>
         <v-col md="6">
             <v-card height="668px">
-                <div class="content_div">
+                <div class="content_div pa-3">
                     흔히 트렌드란 뭔가 재빠르게 세상을 따라가고, 핫플레이스를 좋아하는 패피들로 연상됩니다. 하지만 저는 트렌드를, 팀원과의 소통에 관한 관점으로 얘기해보려 합니다. 트렌드는 어떠한 방향으로 쏠리는 사회적 추세, 경향을 뜻합니다. 소통은 사회성, 사람들과의 커뮤니케이션 활동입니다. 트렌드와 소통을 같은 선상에 놓고 비교하면, 트렌드는 앞서 나가는 멋진 사람에 대한 좁은 정의 뿐만 아니라, 사람(팀원)들이 자주 사용하는 언어와 행동, 도구를 파악하는 넓은 의미로도 비춰질 수 있습니다. 사람들과 더 잘 대화하고 함께 가기 위해 그들의 행동과 방향성을 읽고 공감하며, 그들의 언어로 대화를 나누는 것도 트렌드를 읽는 것입니다.
                 </div>
             </v-card>
@@ -30,6 +30,7 @@
                 >
                     <v-row v-if="item.userNickname!=me" cols="12" justify="left">
                         <v-col md="10">
+                            <span>{{item.userNickname}}</span>
                             <v-card flat style="background-color: #ECDACE" width="70%">
                                 <v-card-text class="ma-0 pa-2">
                                     {{item.text}}
@@ -67,6 +68,7 @@
                 >
                     <v-row v-if="item.userName!=me" cols="12" justify="left">
                         <v-col md="10">
+                            <span>{{item.userName}}</span>
                             <v-card flat style="background-color: #ECDACE" width="70%">
                                 <v-card-text class="ma-0 pa-2">
                                     {{item.content}}
